a4cfb2579907aa73846345f488bddaec6c04077a
Tay West <tayanne.west@gmail.com>
2022-02-03 15:28:35 -0500

    get some info and card styles on here

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -13,25 +13,55 @@ const Card = styled.div`
   justify-content: center;
   align-content: center;
   z-index: 999;
+
+  h1, h2 {
+    text-align: center;
+  }
+`
+
+const Blurb = styled.div`
+  width: 500px;
+  height: 300px;
+  background-color: white;
+  border-radius: 10px;
+  border-style: outset;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  margin: auto;
+  z-index: 999;
+  h1 {
+    text-align: center;
+  }
+`
+
+const Info = styled.div`
+  width: 30vw;
+  height: 300px;
+  background-color: white;
+  border-radius: 10px;
+  border-style: outset;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  margin: auto;
+  z-index: 999;
+
+  h1 {
+    text-align: left;
+  }
 `
 
 const GridContainer = styled.div`
   display: grid;
-  width: 100%;
+  // width: 100fr;
   z-index: 999;
   grid-gap: 10px;
   grid-template-columns: repeat(1, 1fr);
   &::-webkit-scrollbar{
         display: none;
     };
-
-  h1 {
-    text-align: center;
-  }
-
-  h2 {
-    text-align: center;
-  }
 `
 
 const Contact = () => {
@@ -47,6 +77,33 @@ const Contact = () => {
               </Link>
             </h2>
           </Card>
+          <h1>Hello</h1>
+          <Blurb>
+            <h1>Meet the devs yeah</h1>
+          </Blurb>
+          <Info>
+            <h1>Nate</h1>
+            <p>Here goes a little personal info before the contact info</p>
+            <ul>
+              <li>Portfolio</li>
+              <li>GitHub</li>
+              <li>Email</li>
+            </ul>
+          </Info>
+          <Info>
+            <h1>Jeremy</h1>
+            <p>Here goes a little personal info before the contact info</p>
+              <li>Portfolio</li>
+              <li>GitHub</li>
+              <li>Email</li>
+          </Info>
+          <Info>
+            <h1>Tay</h1> 
+            <p>Here goes a little personal info before the contact info</p>
+              <li>Portfolio</li>
+              <li>GitHub</li>
+              <li>Email</li>
+          </Info>
         </GridContainer>
       </div>
     )
